47c28a814e0eb4bec669adf380bf7fc07eff53e6
unknown <tfb@g3c41.windows.cin.ufpe.br>
2018-12-10 11:33:13 -0300

    Progress on filtering; added template

--- NurseManager/src/app/Busca/busca.component.ts
+++ NurseManager/src/app/Busca/busca.component.ts
@@ -1,8 +1,9 @@
-import { Component, OnInit} from '@angular/core';
-import { NgModule} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NgModule } from '@angular/core';
 
-import {Enfermeiro} from '../Model/enfermeiro';
-import {EnfermeiroService} from '../Model/enfermeiro.service';
+import { Enfermeiro } from '../model/enfermeiro';
+import { EnfermeiroService } from '../model/enfermeiro.service';
+import { SetorService } from '../model/setor.service';
 
 @Component({
     selector: 'busca',
@@ -15,10 +16,17 @@ export class BuscaComponent implements OnInit{
 
     nameSelect: string;
     specSelect: string;
-    specList: string[];
+    specOption: string[];
+    sectorSelect: string;
+    sectorOption: string[];
+    shiftSelect: string;
+    shiftOption: string[];
+    liaisonSelect: string;
+    liaisonOption: string[];
 
     constructor(
-        private enfService: EnfermeiroService
+        private enfService: EnfermeiroService,
+        private setService: SetorService
     ) {}
 
     nameFilter(enf: Enfermeiro, term: string):boolean{
@@ -37,19 +45,49 @@ export class BuscaComponent implements OnInit{
         }
     }
 
-    /*sectorFilter(enf: Enfermeiro, term: string):boolean{
+    sectorFilter(enf: Enfermeiro, term: string):boolean{
         if (term.length > 0){
-            return enf.setor.especialidadeList .find(spec => spec.nome === term);
+            return enf.setor.especialidadeSetor === term;
         } else {
             return true;
         }
-    }*/
+    }
+
+    shiftFilter(enf: Enfermeiro, term: string):boolean{
+        if (term.length > 0){
+            return enf.turno.nome === term;
+        } else {
+            return true;
+        }
+    }
+
+    liaisonFilter(enf: Enfermeiro, term: string):boolean{
+        if (term.length > 0){
+            return enf.vinculo === term;
+        } else {
+            return true;
+        }
+    }
 
     onSearch(): void {
-        this.filtList
+        this.filtList = this.enfList
+        .filter(enf => this.nameFilter(enf,this.nameSelect))
+        .filter(enf => this.specFilter(enf,this.specSelect))
+        .filter(enf => this.sectorFilter(enf,this.sectorSelect))
+        .filter(enf => this.shiftFilter(enf,this.shiftSelect))
+        .filter(enf => this.liaisonFilter(enf,this.liaisonSelect));
     }
 
     ngOnInit(): void{
         this.enfList = this.enfService.getEnfermeiros();
+    }
+
+    listSpecs(specs):string{
+        var result = "";
+        result = result.concat(specs[0].nome);
+        for(let i = 1; i < specs.length; i++){
+            result = result.concat(`;${specs[i].nome}`);
+        }
+        return result;
     }
 }
